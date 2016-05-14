1aa164ff7bfb6949c511d1987aead9cb0101a3a4
Guido L <lipkegu@gmail.com>
2016-05-14 17:22:14 +0200

    Radiostatus: Add missing Tables

--- infusions/gr_radiostatus_panel/infusion.php
+++ infusions/gr_radiostatus_panel/infusion.php
@@ -34,34 +34,39 @@ $inf_weburl = 'http://www.granade.eu';
 $inf_folder = 'gr_radiostatus_panel';
 
 $inf_newtable[1] = DB_GR_RADIOSTATUS." (
-rs_id							SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+rs_id				SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
 rs_server_typ			TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
 rs_server_id			SMALLINT(5) UNSIGNED NOT NULL DEFAULT '1',
-rs_name						VARCHAR(20) NOT NULL DEFAULT 'Stream',
-rs_ip							VARCHAR(255) NOT NULL DEFAULT '0.0.0.0',
-rs_port						VARCHAR(5) NOT NULL DEFAULT '8000',
-rs_pw							VARCHAR(255) NOT NULL DEFAULT '',
-rs_apw						VARCHAR(255) NOT NULL DEFAULT '',
-rs_ps							VARCHAR(255) NOT NULL DEFAULT '',
-rs_tele						TEXT NOT NULL,
-rs_flash					VARCHAR(255) NOT NULL DEFAULT '',
-rs_cache					VARCHAR(255) NOT NULL DEFAULT '',
-rs_usertyp				TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
-rs_theme					TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
-rs_panel					TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
-rs_order					SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0',
-rs_status					TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+rs_name				VARCHAR(20) NOT NULL DEFAULT 'Stream',
+rs_ip				VARCHAR(255) NOT NULL DEFAULT '0.0.0.0',
+rs_transip			VARCHAR(255) NOT NULL DEFAULT '0.0.0.0',
+rs_port				VARCHAR(5) NOT NULL DEFAULT '8000',
+rs_transport			VARCHAR(5) NOT NULL DEFAULT '7999',
+rs_pw				VARCHAR(255) NOT NULL DEFAULT '',
+rs_apw				VARCHAR(255) NOT NULL DEFAULT '',
+rs_transpw			VARCHAR(255) NOT NULL DEFAULT '',
+rs_ps				VARCHAR(255) NOT NULL DEFAULT '',
+rs_tele				TEXT NOT NULL,
+rs_flash				VARCHAR(255) NOT NULL DEFAULT '',
+rs_cache				VARCHAR(255) NOT NULL DEFAULT '',
+rs_usertyp			TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
+rs_theme				TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
+rs_panel				TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
+rs_order				SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0',
+rs_gbintervall		SMALLINT(5) UNSIGNED NOT NULL DEFAULT '3',
+rs_status			TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
 rs_status_gb			TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
 rs_status_gb_dj		TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+rs_streamcenter		TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
 rs_status_boxen		TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
-rs_gb_max					SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0',
+rs_gb_max			SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0',
 rs_gb_max_user		SMALLINT(5) UNSIGNED NOT NULL DEFAULT '1',
-rs_gb_popup				TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
-rs_access					TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-rs_access_l				TINYINT(3) UNSIGNED NOT NULL DEFAULT '102',
-rs_access_a				TINYINT(3) UNSIGNED NOT NULL DEFAULT '102',
+rs_gb_popup			TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+rs_access			TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+rs_access_l			TINYINT(3) UNSIGNED NOT NULL DEFAULT '102',
+rs_access_a			TINYINT(3) UNSIGNED NOT NULL DEFAULT '102',
 rs_access_gb			TINYINT(3) UNSIGNED NOT NULL DEFAULT '102',
-rs_access_gb_user	TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+rs_access_gb_user		TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
 PRIMARY KEY (rs_id),
 INDEX rs_panel (rs_panel, rs_access, rs_status),
 INDEX rs_boxen (rs_access, rs_status, rs_status_boxen),
@@ -69,31 +74,31 @@ INDEX rs_boxen_id (rs_id, rs_access, rs_status, rs_status_boxen)
 ) ENGINE=MyISAM;";
 
 $inf_newtable[2] = DB_GR_RADIOSTATUS_GRUSSBOX." (
-rsgb_id						BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-rsgb_stream				SMALLINT(5) NOT NULL DEFAULT '1',
+rsgb_id				BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+rsgb_stream			SMALLINT(5) NOT NULL DEFAULT '1',
 rsgb_user_id			MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0',
 rsgb_user_ip			VARCHAR(255) NOT NULL DEFAULT '0.0.0.0',
 rsgb_user_name		VARCHAR(255) NOT NULL DEFAULT '',
 rsgb_user_ort			VARCHAR(255) NOT NULL DEFAULT '',
-rsgb_title				VARCHAR(255) NOT NULL DEFAULT '',
+rsgb_title			VARCHAR(255) NOT NULL DEFAULT '',
 rsgb_interpret		VARCHAR(255) NOT NULL DEFAULT '',
-rsgb_gruss				TEXT NOT NULL,
-rsgb_time					INT(10) NOT NULL DEFAULT '0',
-rsgb_status				TINYINT(1) NOT NULL DEFAULT '0',
+rsgb_gruss			TEXT NOT NULL,
+rsgb_time			INT(10) NOT NULL DEFAULT '0',
+rsgb_status			TINYINT(1) NOT NULL DEFAULT '0',
 PRIMARY KEY (rsgb_id)
 ) ENGINE=MyISAM;";
 
 $inf_newtable[3] = DB_GR_RADIOSTATUS_ACTION." (
-rsa_stream				SMALLINT(5) NOT NULL DEFAULT '1',
-rsa_user_id				MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '1',
-rsa_sction				TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
-rsa_time					INT(10) UNSIGNED NOT NULL DEFAULT '0'
+rsa_stream			SMALLINT(5) NOT NULL DEFAULT '1',
+rsa_user_id			MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '1',
+rsa_sction			TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
+rsa_time				INT(10) UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM;";
 
 $inf_newtable[4] = DB_GR_RADIOSTATUS_TITLE." (
 rst_interpret			VARCHAR(255) NOT NULL DEFAULT '',
-rst_title					VARCHAR(255) NOT NULL DEFAULT '',
-rst_time					INT(10) UNSIGNED NOT NULL DEFAULT '0'
+rst_title			VARCHAR(255) NOT NULL DEFAULT '',
+rst_time				INT(10) UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM;";
 
 $inf_droptable[1] = DB_GR_RADIOSTATUS;
